355cf3dcaa133de2c85d75035da58f9d9c41c03a
nickolasmainetti <nickolas.mainetti@edu.senai.br>
2026-08-05 09:22:38 -0300

    mais conteudo

---
+++ atividade01/condicao.php
@@ -0,0 +1,11 @@
+<?php
+    /*Crie um codigo utilizando as condições aprendidas em sala,
+     Desenvolva uma pagina capaz de calcular
+      o dobro de um numero */
+
+    $numero = 30;
+    $numero2 = 20;
+    $dobro = $numero * 2;
+    $dobro2 = $numero2 * 2;
+    echo "O dobro de $numero é $dobro e o dobro de $numero2 é $dobro2";
+?>
