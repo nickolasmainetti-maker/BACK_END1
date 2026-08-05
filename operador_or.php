438d7f133a15292c045c55b2b2b017b8d017b83e
nickolasmainetti <nickolas.mainetti@edu.senai.br>
2026-08-05 08:41:54 -0300

    mais codigos adicionados.

---
+++ operador_or.php
@@ -0,0 +1,11 @@
+<?php
+$feriado = false;
+$fimDesemana = true;
+
+if ($feriado || $fimDesemana) {
+    echo "Pode descansar.";
+} else {
+    echo "Dia util de trabalho.";
+}
+?>
+
---
+++ php_com_html.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <?php echo("php com html"); ?>
+
+    </body>
+</html>
+
