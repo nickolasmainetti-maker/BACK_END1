f11d331d62494f2522113eb71b8392b0fb2b0208
nickolasmainetti <nickolas.mainetti@edu.senai.br>
2026-08-05 10:27:30 -0300

    mais codigos  adicionados ao conjunto

---
+++ atividade01/tabuada.php
@@ -0,0 +1,13 @@
+<?php
+
+    /*Faça um codigo em que calcule a tabuada do numero indicado em uma variavel com o nome de $tabuada, aplique o laço for */
+
+    $tabuada = 4;
+
+    for ($i = 1; $i <= 10; $i ++){           
+
+ echo "Numero: $tabuada * $i " . "=" . $i * $tabuada . "<br>";
+
+
+}
+?>
